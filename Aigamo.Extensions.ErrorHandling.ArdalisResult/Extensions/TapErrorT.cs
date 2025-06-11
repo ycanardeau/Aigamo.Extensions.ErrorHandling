1e0b8116240a78c5b6070ac936b38fd5eaaf615b
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-06-11 22:35:30 +0900

    Create TapErrorT

---
+++ Aigamo.Extensions.ErrorHandling.ArdalisResult/Extensions/TapErrorT.cs
@@ -0,0 +1,44 @@
+using Ardalis.Result;
+
+namespace Aigamo.Extensions.ErrorHandling.ArdalisResult.Extensions;
+
+public static partial class ResultExtensions
+{
+	public static Result<T> TapError<T>(this Result<T> source, Action/* TODO: <> */ error)
+	{
+		if (source.IsNotSuccess())
+		{
+			error();
+		}
+		return source;
+	}
+
+	public static async Task<Result<T>> TapError<T>(this Task<Result<T>> source, Action/* TODO: <> */ error)
+	{
+		var result = await source.ConfigureAwait(false);
+		if (result.IsNotSuccess())
+		{
+			error();
+		}
+		return result;
+	}
+
+	public static async Task<Result<T>> TapError<T>(this Result<T> source, Func<Task>/* TODO: <> */ error)
+	{
+		if (source.IsNotSuccess())
+		{
+			await error().ConfigureAwait(false);
+		}
+		return source;
+	}
+
+	public static async Task<Result<T>> TapError<T>(this Task<Result<T>> source, Func<Task>/* TODO: <> */ error)
+	{
+		var result = await source.ConfigureAwait(false);
+		if (result.IsNotSuccess())
+		{
+			await error().ConfigureAwait(false);
+		}
+		return result;
+	}
+}
