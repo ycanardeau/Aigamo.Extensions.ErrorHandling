81f45a3bee5bcc7bca6482c91ff05a3a4d97d17d
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2025-06-11 22:06:51 +0900

    Create Tap

---
+++ Aigamo.Extensions.ErrorHandling.ArdalisResult/Extensions/Tap.cs
@@ -0,0 +1,44 @@
+using Ardalis.Result;
+
+namespace Aigamo.Extensions.ErrorHandling.ArdalisResult.Extensions;
+
+public static partial class ResultExtensions
+{
+	public static Result Tap(this Result source, Action ok)
+	{
+		if (source.IsSuccess)
+		{
+			ok();
+		}
+		return source;
+	}
+
+	public static async Task<Result> Tap(this Task<Result> source, Action ok)
+	{
+		var result = await source.ConfigureAwait(false);
+		if (result.IsSuccess)
+		{
+			ok();
+		}
+		return result;
+	}
+
+	public static async Task<Result> Tap(this Result source, Func<Task> ok)
+	{
+		if (source.IsSuccess)
+		{
+			await ok().ConfigureAwait(false);
+		}
+		return source;
+	}
+
+	public static async Task<Result> Tap(this Task<Result> source, Func<Task> ok)
+	{
+		var result = await source.ConfigureAwait(false);
+		if (result.IsSuccess)
+		{
+			await ok().ConfigureAwait(false);
+		}
+		return result;
+	}
+}
